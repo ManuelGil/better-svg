d34bbb07884a76ee8b7aef95f20d8abafb0ba59b
midudev <miduga@gmail.com>
2025-10-20 10:53:38 +0200

    Add esbuild configuration and problem matcher plugin

---
+++ esbuild.js
@@ -0,0 +1,71 @@
+/**
+ * Copyright 2025 Miguel Ángel Durán
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+const esbuild = require('esbuild')
+
+const production = process.argv.includes('--production')
+const watch = process.argv.includes('--watch')
+
+/**
+ * @type {import('esbuild').Plugin}
+ */
+const esbuildProblemMatcherPlugin = {
+  name: 'esbuild-problem-matcher',
+
+  setup (build) {
+    build.onStart(() => {
+      console.log('[watch] build started')
+    })
+    build.onEnd(result => {
+      result.errors.forEach(({ text, location }) => {
+        console.error(`✘ [ERROR] ${text}`)
+        if (location == null) return
+        console.error(`    ${location.file}:${location.line}:${location.column}:`)
+      })
+      console.log('[watch] build finished')
+    })
+  }
+}
+
+async function main () {
+  const ctx = await esbuild.context({
+    entryPoints: ['src/extension.ts'],
+    bundle: true,
+    format: 'cjs',
+    minify: production,
+    sourcemap: !production,
+    sourcesContent: false,
+    platform: 'node',
+    outfile: 'dist/extension.js',
+    external: ['vscode'],
+    logLevel: 'warning',
+    plugins: [
+      /* add to the end of plugins array */
+      esbuildProblemMatcherPlugin
+    ]
+  })
+  if (watch) {
+    await ctx.watch()
+  } else {
+    await ctx.rebuild()
+    await ctx.dispose()
+  }
+}
+
+main().catch(e => {
+  console.error(e)
+  process.exit(1)
+})
